11b3d20357715efe68b1371b5d17d35e36a9fe6d
arbazajaz <90388222+arbazajaz@users.noreply.github.com>
2023-09-04 00:51:53 +0500

    apiresource routes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 // use Illuminate\Console\View\Components\Task;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
 use App\Http\Resources\TaskCollection;
 
 class TaskController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         return new TaskCollection(Task::all());
         
+    }
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
     }
 }
